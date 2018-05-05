4f0cbe5d76ca1fdcad119e15c3c83fe4e1f5d8f5
AmmarahCassim <34282478+AmmarahCassim@users.noreply.github.com>
2018-05-05 22:02:43 +0200

    Update test1.js

--- test files/test1.js
+++ test files/test1.js
@@ -1,12 +1,12 @@
  beforeEach(function() {
     if (process.env.SAUCE_USERNAME != undefined) {
       this.browser = new webdriver.Builder()
-      .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
+      .usingServer('http://'+ process.env.AmmarahCassim+':'+process.env.24302255-f2d7-46f5-ac1c-9c90ec0eff11+'@ondemand.saucelabs.com:80/wd/hub')
       .withCapabilities({
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         build: process.env.TRAVIS_BUILD_NUMBER,
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY,
+        username: process.env.AmmarahCassim,
+        accessKey: process.env.24302255-f2d7-46f5-ac1c-9c90ec0eff11,
         browserName: "chrome"
       }).build();
     } else {
